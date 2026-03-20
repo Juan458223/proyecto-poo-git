37f30f4aeba49c00cf9f181731feaf99c59ca917
Juan458223 <judrodriguezf@udistrital.edu.co>
2026-03-20 15:57:01 -0500

    feat: declara clase Mago, con constructor y atributos

---
+++ Mago.java
@@ -0,0 +1,12 @@
+Public Class Mago{
+	int rango;
+	float mana;
+	String elemento;
+public Mago(int rango, float mana, String elemento){
+
+	this.rango=rango;
+	this.mana=mana;
+	this.elemento=elemento;
+}
+
+}
