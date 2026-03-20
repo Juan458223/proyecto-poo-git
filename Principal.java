c80f17aad1dfa74673d6d8e59efb8898f59faa34
JDRFdev <2305juanda@gmail.com>
2026-03-20 14:43:21 -0500

    feat:agrega declaración de la case Principal y su respectiva logica implementado la calse Proyecto

---
+++ Principal.java
@@ -0,0 +1,17 @@
+public class Principal {
+    public static void main(String[] args) {
+        Proyecto proyecto = new Proyecto("Sistema de Gestión");
+
+        Tarea t1 = new Tarea(1, "Diseñar base de datos", "Crear tablas y relaciones");
+        Tarea t2 = new Tarea(2, "Implementar API", "Desarrollar endpoints REST");
+        Tarea t3 = new Tarea(3, "Crear interfaz gráfica", "Diseñar pantallas en JavaFX");
+
+        proyecto.agregarTarea(t1);
+        proyecto.agregarTarea(t2);
+        proyecto.agregarTarea(t3);
+
+        t2.completar();
+
+        proyecto.mostrarTareas();
+    }
+}
