89b6e74dcef607916b5f332c83edacec7ac7348b
JDRFdev <2305juanda@gmail.com>
2026-03-20 14:33:13 -0500

    feat: agrega contructor, metodo agragar tarea y metodo mostrar tareas de la clase Proyecto

--- Proyecto.java
+++ Proyecto.java
@@ -1,8 +1,26 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Proyecto {
     private String nombre;
     private List<Tarea> tareas;
 
+    public Proyecto(String nombre) {
+        this.nombre = nombre;
+        this.tareas = new ArrayList<>();
+    }
 
+    public void agregarTarea(Tarea tarea) {
+        tareas.add(tarea);
+    }
+
+    public void mostrarTareas() {
+        System.out.println("Proyecto: " + nombre);
+        for (Tarea t : tareas) {
+            System.out.println("- ID: " + t.getId() +
+                               " | Nombre: " + t.getNombre() +
+                               " | Contenido: " + t.getContenido() +
+                               " | Completada: " + t.isCompletada());
+        }
+    }
 }
